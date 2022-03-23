5e423e098a447bc87500951134fb0291e6367d0d
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2022-03-23 11:23:15 -0300

    Update client.py

--- client.py
+++ client.py
@@ -21,15 +21,3 @@ class Client:
     
     def send_msg(self):
         self.tcp.send(str.encode(json.dumps(self.create_msg())))
-
-if __name__ == "__main__":
-    client = Client()
-    client.connect()
-    trash = {"porcentage": 10, "locked": False}
-    while not trash["locked"]:
-        client.send_msg(trash)
-        if trash["porcentage"] != 80:
-            trash["porcentage"]+= 10
-        else:
-            trash["locked"] = True
-    client.send_msg(trash)
